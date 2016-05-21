c20663a4150824e96701b792910f1a30834fb9a4
John Raymund Catahay <raymund.cat@gmail.com>
2016-05-22 03:46:31 +0800

    added camera and gallery utilities

--- app/src/main/java/com/example/raymundcat/safetycj/MainActivity.java
+++ app/src/main/java/com/example/raymundcat/safetycj/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.raymundcat.safetycj;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
@@ -22,6 +27,11 @@ import org.androidannotations.annotations.ViewById;
 @EActivity(R.layout.activity_main)
 public class MainActivity extends Activity {
 
+    private static final int SELECT_PICTURE = 1;
+    private static final int CAMERA_REQUEST = 2;
+
+    private String selectedImagePath;
+
     @ViewById(R.id.main_toolbar_burger)
     Button burgerButton;
 
@@ -36,6 +46,9 @@ public class MainActivity extends Activity {
 
     @ViewById(R.id.home_text_incident)
     TextView incidentTitle;
+
+    @ViewById(R.id.home_text_attach)
+    TextView attachTitle;
 
     @Click(R.id.home_button_catcall)
     void didPressButtonCatcall(){
@@ -71,6 +84,59 @@ public class MainActivity extends Activity {
             buttonStalking.setSelected(false);
             buttonEnvironment.setSelected(true);
         }
+    }
+
+    @Click(R.id.home_button_camera)
+    void didPressCamera(){
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA_REQUEST);
+    }
+
+    @Click(R.id.home_button_gallery)
+    void didPressGallery(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent,
+                "Select Picture"), SELECT_PICTURE);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == SELECT_PICTURE) {
+                Uri selectedImageUri = data.getData();
+                selectedImagePath = getPath(selectedImageUri);
+                attachTitle.setText("Attachment: Photo from Gallery");
+            }
+        }
+
+        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            attachTitle.setText("Attachment: Photo from Camera");
+        }
+    }
+
+    /**
+     * helper to retrieve the path of an image URI
+     */
+    public String getPath(Uri uri) {
+        // just some safety built in
+        if( uri == null ) {
+            // TODO perform some logging or show user feedback
+            return null;
+        }
+        // try to retrieve the image from the media store first
+        // this will only work for images selected from gallery
+        String[] projection = { MediaStore.Images.Media.DATA };
+        Cursor cursor = managedQuery(uri, projection, null, null, null);
+        if( cursor != null ){
+            int column_index = cursor
+                    .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        }
+        // this is our fallback here
+        return uri.getPath();
     }
 
     @AfterViews
