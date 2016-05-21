df3ebcca1b3d910d23927767a370e4c3cd02f551
John Raymund Catahay <raymund.cat@gmail.com>
2016-05-21 19:41:24 +0800

    renewed layout of all the things

--- app/src/main/java/com/example/raymundcat/safetycj/MainActivity.java
+++ app/src/main/java/com/example/raymundcat/safetycj/MainActivity.java
@@ -1,52 +1,40 @@
 package com.example.raymundcat.safetycj;
 
+import android.app.Activity;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.raymundcat.safetycj.activities.MapActivity_;
 import com.example.raymundcat.safetycj.fragments.MapFragment;
 import com.example.raymundcat.safetycj.fragments.ReportFragment;
 
 import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
 
 @EActivity(R.layout.activity_main)
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends Activity {
 
-    ScreenSlidePagerAdapter mScreenSlidePagerAdapter;
-    ViewPager mViewPager;
+    @ViewById(R.id.main_toolbar_burger)
+    Button burgerButton;
 
     @AfterViews
     void afterViews(){
-        mScreenSlidePagerAdapter =
-                new ScreenSlidePagerAdapter(
-                        getSupportFragmentManager());
-        mViewPager = (ViewPager) findViewById(R.id.pager);
-        mViewPager.setAdapter(mScreenSlidePagerAdapter);
+
     }
 
-    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
-        public ScreenSlidePagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            switch (position) {
-                case 0:
-                    return MapFragment.getInstance();
-                case 1:
-                    return ReportFragment.getInstance();
-                default:
-                    return MapFragment.getInstance();
-            }
-        }
-
-        @Override
-        public int getCount() {
-            return 2;
-        }
+    @Click(R.id.main_toolbar_burger)
+    void didPressMainBurger(){
+        MapActivity_.intent(this).start();
+        Log.i("","im clicked");
     }
+
+
 }
---
+++ app/src/main/java/com/example/raymundcat/safetycj/activities/MapActivity.java
@@ -0,0 +1,15 @@
+package com.example.raymundcat.safetycj.activities;
+
+import android.app.Activity;
+
+import com.example.raymundcat.safetycj.R;
+
+import org.androidannotations.annotations.EActivity;
+
+/**
+ * Created by Raymund on 21/05/2016.
+ */
+@EActivity(R.layout.activity_map)
+public class MapActivity extends Activity{
+
+}
