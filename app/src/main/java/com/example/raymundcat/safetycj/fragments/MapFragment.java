e686b3be3ff658444fc58ce6c47895a8fd404050
John Raymund Catahay <raymund.cat@gmail.com>
2016-05-21 17:42:50 +0800

    added google play services, and dummy map view

--- app/src/main/java/com/example/raymundcat/safetycj/fragments/MapFragment.java
+++ app/src/main/java/com/example/raymundcat/safetycj/fragments/MapFragment.java
@@ -10,7 +10,7 @@ import org.androidannotations.annotations.EFragment;
  * Created by Raymund on 21/05/2016.
  */
 @EFragment(R.layout.fragment_map)
-public class MapFragment extends Fragment{
+public class MapFragment extends Fragment {
 
     public static MapFragment getInstance(){
         MapFragment fragment = new MapFragment_();
